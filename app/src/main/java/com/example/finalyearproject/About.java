0665199981022fdbd136bae26e14048247e2d11b
NilanshuBasnet <mail.nilanshu@gmail.com>
2021-04-12 08:14:08 +0545

    Info added in help activity

--- app/src/main/java/com/example/finalyearproject/About.java
+++ app/src/main/java/com/example/finalyearproject/About.java
@@ -6,10 +6,11 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class About extends AppCompatActivity {
 
-    Button btnBack;
+    Button btnBack, btnPrivacy, btnUse ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +18,30 @@ public class About extends AppCompatActivity {
 
 
 
+        btnUse = findViewById(R.id.btn_termsOfUse);
+        btnPrivacy = findViewById(R.id.btn_privacy);
         btnBack = findViewById(R.id.btn_back);
+
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+
+        btnPrivacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),  "User will only have access to their data.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnUse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),  "As this app is a FYP project, commercial use is prohibited.   ", Toast.LENGTH_SHORT).show();
             }
         });
     }
